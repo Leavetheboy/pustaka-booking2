a57b569a5fead04224ea50a30f7ebb8b2f87dc5f
Leavetheboy <kasday25@gmail.com>
2022-10-30 16:17:08 +0700

    tugas pert 7

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,23 +2,23 @@
      <h1><?php echo $judul ?></h1>
      <h4>Nama</h4>
      <ul type="disc">
-          <li>Nama Depan : Athariq Byan</li>
-          <li>Nama Belakang : Fadhlan Santoso</li>
+          <li>Nama Depan : Mohamad</li>
+          <li>Nama Belakang : Ali</li>
      </ul>
      <br>
      <h4>Alamat</h4>
      <ul type="none">
-          <li> Jalan Arjuna Gg. 14A No. 26 Slerok Tegal</li>
+          <li> Dukuh Wungu</li>
      </ul>
 
      <h4>Tempat Lahir</h4>
      <ul type="none">
-          <li>Tegal</li>
+          <li>Bandung</li>
      </ul>
 
      <h4>Olah Raga Favorit</h4>
      <ul type="square">
           <li>Bulutangkis</li>
-          <li>Catur</li>
+          <li>Basket</li>
      </ul>
 </section>
